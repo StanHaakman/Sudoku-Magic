046b0e9c56f293f4d6d637cd08de8e1548385689
Stan Haakman <stanhaakman@icloud.com>
2021-06-07 22:55:57 +0200

    Tkinter simple screen opzetten

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import pygame
+import tkinter as tk
 
+
+root = tk.Tk()
+
+root.title('Project Steam Dashboard')
+root.minsize(700, 700)
+
+root.configure()
+root.geometry(str(root.winfo_screenwidth() - 100) + 'x' + str(root.winfo_screenheight() - 100))
+root.mainloop()
