68371bce3ba0b0342bb3c16f3bac60e3e5c78366
Stan Haakman <stanhaakman@icloud.com>
2021-06-09 21:29:08 +0200

    Make functions to check row,columns and subgrids and a surrounding function called valid location. Also make a start on creating a new sudoku board

--- SudokuBoard.py
+++ SudokuBoard.py
@@ -1,3 +1,4 @@
+from random import shuffle
 from tkinter import *
 import numpy as np
 
@@ -18,7 +19,7 @@ class SudokuBoard(object):
 
 	def __create_board(self):
 		# create an initial matrix, or a list of a list
-		board = np.zeros((9, 9))
+		self.board = [[0 for i in range(9)] for j in range(9)]
 
 		'''
 		Create emtpy numpy array with zeros to be filled in
@@ -36,40 +37,61 @@ class SudokuBoard(object):
 		'''
 
 		# iterate over each line
-		for line in board_file:
-			line = line.strip()
-
-			# raise error if line is longer or shorter than 9 characters
-			if len(line) != 9:
-				board = np.array([])
-				raise SudokuError(
-					"Each line in the sudoku puzzle must be 9 chars long."
-				)
-
-			# create a list for the line
-			print(board)
-			np.append(board, [])
-			print(board)
-
-			# then iterate over each character
-			for c in line:
-				# Raise an error if the character is not an integer
-				if not c.isdigit():
-					raise SudokuError(
-						"Valid characters for a sudoku puzzle must be in 0-9"
-					)
-				# Add to the latest list for the line
-				board[-1].append(int(c))
+		# for line in board_file:
+		# 	line = line.strip()
+		#
+		# 	# raise error if line is longer or shorter than 9 characters
+		# 	if len(line) != 9:
+		# 		board = np.array([])
+		# 		raise SudokuError(
+		# 			"Each line in the sudoku puzzle must be 9 chars long."
+		# 		)
+		#
+		# 	# create a list for the line
+		# 	print(board)
+		# 	np.append(board, [])
+		# 	print(board)
+		#
+		# 	# then iterate over each character
+		# 	for c in line:
+		# 		# Raise an error if the character is not an integer
+		# 		if not c.isdigit():
+		# 			raise SudokuError(
+		# 				"Valid characters for a sudoku puzzle must be in 0-9"
+		# 			)
+		# 		# Add to the latest list for the line
+		# 		board[-1].append(int(c))
 
 		# Raise an error if there are not 9 lines
-		if len(board) != 9:
+		if len(self.board) != 9:
 			raise SudokuError("Each sudoku puzzle must be 9 lines long")
 
 		# Return the constructed board
-		return board
+		return self.board
 
-	def __generate_solution(self, empty_grid):
-		pass
+	def __generate_solution(self):
+		"""generates a full solution with backtracking"""
+		grid = self.board
+		number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+		for i in range(0, 81):
+			row = i // 9
+			col = i % 9
+			# find next empty cell
+			if grid[row][col] == 0:
+				shuffle(number_list)
+				for number in number_list:
+					if self.valid_location(grid, row, col, number):
+						self.path.append((number, row, col))
+						grid[row][col] = number
+						if not self.find_empty_square(grid):
+							return True
+						else:
+							if self.generate_solution(grid):
+								# if the grid is full
+								return True
+				break
+		grid[row][col] = 0
+		return False
 
 	def create_empty_grid(self):
 		"""
@@ -92,12 +114,66 @@ class SudokuBoard(object):
 
 		new_frame.pack()
 
-	def fill_grid(self):
+	def number_in_row(self, grid, row, number):
 		"""
-		Own function
+		Returns true if the number is used in the given row
+		:param grid:
+		:param col:
+		:param number:
 		:return:
 		"""
-		pass
+		if number in grid[row]:
+			return True
+		return False
+
+	def number_in_column(self, grid, col, number):
+		"""
+		Returns true if the number is used in the given column
+		:param grid:
+		:param col:
+		:param number:
+		:return:
+		"""
+		for i in range(9):
+			if grid[i][col] == number:
+				return True
+		return False
+
+	def number_in_subgrid(self, grid, row, col, number):
+		"""
+		Return true if the number is used in the given subgrid
+		:param grid:
+		:param row:
+		:param col:
+		:param number:
+		:return:
+		"""
+		sub_row = (row // 3) * 3
+		sub_col = (col // 3) * 3
+
+		for i in range(sub_row, (sub_row + 3)):
+			for j in range(sub_col, (sub_col + 3)):
+				if grid[i][j] == number:
+					return True
+		return False
+
+	def valid_location(self, grid, row, col, number):
+		"""
+		Return false if the location of the given number is invalid
+		:param grid:
+		:param row:
+		:param col:
+		:param number:
+		:return:
+		"""
+
+		if self.number_in_row(grid, row, number):
+			return False
+		elif self.number_in_column(grid, col, number):
+			return False
+		elif self.number_in_subgrid(grid, row, col, number):
+			return False
+		return True
 
 	def run_application(self):
 		"""
@@ -105,3 +181,4 @@ class SudokuBoard(object):
 		:return:
 		"""
 		self.root.mainloop()
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 
+import numpy as np
+
 from SudokuBoard import SudokuBoard
 
 gameboard = SudokuBoard(root = tk.Tk())
-
 
 # gameboard.create_empty_grid()
 gameboard.run_application()
