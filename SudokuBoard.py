8282dd8a293304cbc5709de3826b2230bb4fc86b
Stan Haakman <stanhaakman@icloud.com>
2021-06-10 16:22:48 +0200

    Finish sudoku board and the generation. Also refactor the class to only contain code for board creation

--- SudokuBoard.py
+++ SudokuBoard.py
@@ -1,21 +1,13 @@
 from random import shuffle
-from tkinter import *
-import numpy as np
 
 from SudokuError import SudokuError
-
-BOARDS = ['debug', 'n00b', 'l33t', 'error']  # Available sudoku boards
-MARGIN = 20  # Pixels around the board
-SIDE = 50  # Width of every board cell.
-WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9  # Width and height of the whole board
 
 
 class SudokuBoard(object):
 	def __init__(self, root):
-		self.root = root
-		self.root.geometry(str(WIDTH) + "x" + str(HEIGHT))
-		self.root.title('Sudoku Board')
-		self.__create_board()
+		self.counter = 0
+		self.path = []
+		self.board = self.__create_board()
 
 	def __create_board(self):
 		# create an initial matrix, or a list of a list
@@ -35,6 +27,8 @@ class SudokuBoard(object):
 			[0. 0. 0. 0. 0. 0. 0. 0. 0.]
 		]
 		'''
+
+		self.__generate_solution(self.board)
 
 		# iterate over each line
 		# for line in board_file:
@@ -66,83 +60,64 @@ class SudokuBoard(object):
 		if len(self.board) != 9:
 			raise SudokuError("Each sudoku puzzle must be 9 lines long")
 
+		for row in self.board:
+			print(row)
+
 		# Return the constructed board
 		return self.board
 
-	def __generate_solution(self):
+	def __generate_solution(self, board):
 		"""generates a full solution with backtracking"""
-		grid = self.board
 		number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 		for i in range(0, 81):
 			row = i // 9
 			col = i % 9
 			# find next empty cell
-			if grid[row][col] == 0:
+			if board[row][col] == 0:
 				shuffle(number_list)
 				for number in number_list:
-					if self.valid_location(grid, row, col, number):
+					if self.__valid_location(board, row, col, number):
 						self.path.append((number, row, col))
-						grid[row][col] = number
-						if not self.find_empty_square(grid):
+						board[row][col] = number
+						if not self.__find_empty_spot(board):
 							return True
 						else:
-							if self.generate_solution(grid):
-								# if the grid is full
+							if self.__generate_solution(board):
+								# if the board is full
 								return True
 				break
-		grid[row][col] = 0
+		board[row][col] = 0
 		return False
 
-	def create_empty_grid(self):
-		"""
-		Own function
-		:return:
-		"""
-		new_frame = Frame(self.root, width = self.root.winfo_screenwidth())
-		total = 0
-
-		# Loop through grid in three rows
-		for row_index in range(3):
-			# Loop through row in three columns
-			for column_index in range(3):
-				# Loop through column for 9 slots
-				for grid_index in range(9):
-					# Print row column and grid index for visualization of grid
-					print(row_index, column_index, grid_index)
-					total += 1
-					print(total)
-
-		new_frame.pack()
-
-	def number_in_row(self, grid, row, number):
+	def __number_in_row(self, board, row, number):
 		"""
 		Returns true if the number is used in the given row
-		:param grid:
-		:param col:
+		:param board:
+		:param row:
 		:param number:
 		:return:
 		"""
-		if number in grid[row]:
+		if number in board[row]:
 			return True
 		return False
 
-	def number_in_column(self, grid, col, number):
+	def __number_in_column(self, board, col, number):
 		"""
 		Returns true if the number is used in the given column
-		:param grid:
+		:param board:
 		:param col:
 		:param number:
 		:return:
 		"""
 		for i in range(9):
-			if grid[i][col] == number:
+			if board[i][col] == number:
 				return True
 		return False
 
-	def number_in_subgrid(self, grid, row, col, number):
+	def __number_in_subboard(self, board, row, col, number):
 		"""
-		Return true if the number is used in the given subgrid
-		:param grid:
+		Return true if the number is used in the given subboard
+		:param board:
 		:param row:
 		:param col:
 		:param number:
@@ -153,44 +128,37 @@ class SudokuBoard(object):
 
 		for i in range(sub_row, (sub_row + 3)):
 			for j in range(sub_col, (sub_col + 3)):
-				if grid[i][j] == number:
+				if board[i][j] == number:
 					return True
 		return False
 
-	def valid_location(self, grid, row, col, number):
+	def __valid_location(self, board, row, col, number):
 		"""
 		Return false if the location of the given number is invalid
-		:param grid:
+		:param board:
 		:param row:
 		:param col:
 		:param number:
 		:return:
 		"""
 
-		if self.number_in_row(grid, row, number):
+		if self.__number_in_row(board, row, number):
 			return False
-		elif self.number_in_column(grid, col, number):
+		elif self.__number_in_column(board, col, number):
 			return False
-		elif self.number_in_subgrid(grid, row, col, number):
+		elif self.__number_in_subboard(board, row, col, number):
 			return False
 		return True
 
-	def find_empty_spot(self, grid):
+	def __find_empty_spot(self, board):
 		"""
-		Return the next empty spot in the grid
-		:param grid:
+		Return the next empty spot in the board
+		:param board:
 		:return:
 		"""
 		for i in range(9):
 			for j in range(9):
-				if grid[i][j] == 0:
+				if board[i][j] == 0:
 					return i, j
 		return
-
-	def run_application(self):
-		"""
-		Own function
-		:return:
-		"""
-		self.root.mainloop()
 
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import tkinter as tk
-
-import numpy as np
+import argparse
 
 from SudokuBoard import SudokuBoard
 
+BOARDS = ['debug', 'n00b', 'l33t', 'error']  # Available sudoku boards
+MARGIN = 20  # Pixels around the board
+SIDE = 50  # Width of every board cell.
+WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9  # Width and height of the whole board
+
 gameboard = SudokuBoard(root = tk.Tk())
 
-
-# gameboard.create_empty_grid()
-gameboard.run_application()
 
