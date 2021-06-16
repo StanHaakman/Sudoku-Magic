00d8dfaa61455be5ef427b12e30c29095313c82e
Stan Haakman <stanhaakman@icloud.com>
2021-06-16 12:44:19 +0200

    Add helper function to get unsettled spots. And Finish the solve algoritm

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,4 +1,3 @@
-
 class SudokuSolver(object):
 	def __init__(self, grid):
 		self.grid = grid
@@ -7,7 +6,27 @@ class SudokuSolver(object):
 		pass
 
 	def solve(self):
-		pass
+		"""Solve puzzle with Recursive Backtracking"""
+
+		# todo: Change loop through all to loop through available spots
+		# Loop through all the slots
+		for i in range(0, 81):
+			# Configure the row and column
+			row = i // 9
+			col = i % 9
+
+			# On the next empty cell
+			if self.grid[row][col] == 0:
+				# Loop through the available numbers
+				for number in range(1, 10):
+					# Check if the number has no conflicts
+					if not self.no_conflicts(self.grid, row, col, number):
+						self.grid[row][col] = number
+						if not self.find_unsettled_spot(self):
+							break
+						else:
+							if self.solve:
+								return True
 
 	def no_conflicts(self, grid, row, col, number):
 		"""returns True if the number has been used in that row"""
@@ -27,3 +46,11 @@ class SudokuSolver(object):
 				if grid[i][j] == number:
 					return True
 		return False
+
+	def find_unsettled_spot(self):
+		"""return the next empty square coordinates in the grid"""
+		for i in range(9):
+			for j in range(9):
+				if self.grid[i][j] == 0:
+					return i, j
+		return
