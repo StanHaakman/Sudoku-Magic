ae686193fb42868243b1ca2e3cb434810cf5ec3a
Stan Haakman <stanhaakman@icloud.com>
2021-06-18 11:27:41 +0200

    Remove unused variable

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from tkinter import Tk
 from SudokuGame import SudokuGame
 from SudokuUi import SudokuUI
 
-BOARDS = ['debug', 'n00b', 'l33t', 'error']  # Available sudoku boards
 MARGIN = 20  # Pixels around the board
 SIDE = 50  # Width of every board cell.
 WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9  # Width and height of the whole board
