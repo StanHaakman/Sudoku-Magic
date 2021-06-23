ceef61bff9af3b2443bbc85791c249616475da46
Stan Haakman <stanhaakman@icloud.com>
2021-06-23 21:46:25 +0200

    Write the rapportage further and extend the tests. Also finish the poster

--- codeTests/test_SudokuSolver.py
+++ codeTests/test_SudokuSolver.py
@@ -86,7 +86,7 @@ class TestSudokuSolver(unittest.TestCase):
 		self.assertTrue(testSolver.no_conflicts(0, 0, 4))
 
 	def test_speed(self):
-		total_test = 20
+		total_test = 100
 
 		total_duration = timeit.Timer(lambda: SudokuSolver(SudokuBoard(17).board).solve()).timeit(number = total_test)
 
